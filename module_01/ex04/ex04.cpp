91e1df9fcaf72b227079b64ce8ff0f9905acbf27
Thibaud-43 <thibaud.rouchon@yahoo.fr>
2021-06-23 15:29:07 +0200

    ex04 ok

---
+++ module_01/ex04/ex04.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+int main(void)
+{
+    std::string  string = "HI THIS IS BRAIN";
+    std::string&    stringRef = string;
+    std::string*    stringPtr = &string;
+
+    std::cout << "affichage via reference : " << stringRef << std::endl;
+    std::cout << "affichage via pointeur  : " << *stringPtr << std::endl;
+    return (0);
+}

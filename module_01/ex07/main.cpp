ff5993185017c248b1606d05d9203fc616662cb9
Thibaud ROUCHON <trouchon@e3r7p17.42.fr>
2021-06-24 14:53:29 +0200

    init ex07

---
+++ module_01/ex07/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+	if (argc != 4)
+	{
+		std::cout << "Wrong number of arguments" << std::endl;
+	}
+	
+	return 0;
+}
